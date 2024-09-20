4931f2ea1990f54a355edb825fec645aacb8ff93
Joaquin Kusinsky <JoaquinKusinsky@MBP-de-Joaco.fibertel.com.ar>
2024-09-20 18:11:37 -0300

    mostrar mas peliculas y buscador en screen de populares

--- src/screens/Populares.js
+++ src/screens/Populares.js
@@ -7,7 +7,10 @@ function Populares(){
     return(
         <React.Fragment>
         <h1>Películas más populares:</h1>
-        <Peliculas url= {'https://api.themoviedb.org/3/movie/popular?api_key=9458a99baf5a9ba3fe341cd43217ef95'} cantidadPeliculas = {20}/> 
+        <Peliculas url= {'https://api.themoviedb.org/3/movie/popular?api_key=9458a99baf5a9ba3fe341cd43217ef95'} 
+            mostrarComponenteControlado={true} 
+            cantidadPeliculas = {6}
+            mostrarBotonCargarMas={true} />
         </React.Fragment>
     )
 }
